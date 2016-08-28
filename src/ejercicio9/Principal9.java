51b543bed855310643e6f424bfb648cdf94fdb23
Piter <Piter@DESKTOP-QV720H2>
2016-08-28 00:00:06 -0500

    Version

---
+++ src/ejercicio9/Principal9.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio9;
+
+/**
+ *
+ * @author Piter
+ */
+public class Principal9 {
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Ejercicio9 numeros=new Ejercicio9();
+        
+        numeros.ciclo();
+    }
+    
+    
+}
